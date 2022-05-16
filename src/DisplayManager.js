28ec22ac8424b4676c53288fe8ee8337f0667842
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-05-16 04:40:30 -0400

    Draw entry date and wake and bed time

--- src/DisplayManager.js
+++ src/DisplayManager.js
@@ -24,3 +24,72 @@ function createSymbolButtonString(symbolName){
             <img src="src/Assets/Sprites/${symbolName}.png" />
         </button>`;
 }
+
+//TODO: perhaps move to a drawing script
+function repaintEntryCanvas(entry, textSize){
+    if (!entry){
+        console.error("Invalid entry!", entry);
+        return;
+    }
+    textSize ??= 30;
+    let canvas = $("cvsEntry");
+    let ctx = canvas.getContext("2d");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    let size = (canvas.width < canvas.height)
+        ? canvas.width
+        : canvas.height;
+    //Clear rect
+    ctx.clearRect(0,0,size,size);
+    //Header text
+    ctx.font = `${textSize}px Consolas`;
+    ctx.fillText(
+        `${entry.date} ${(""+entry.wake.time).padStart(2,"0")} ${(""+entry.bed.time).padStart(2,"0")}`,
+        0,
+        textSize
+    );
+    //Circles around wake time
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(
+        198*textSize/30,//x
+        20*textSize/30,//y
+        22*textSize/30,//radius
+        0,
+        2*Math.PI
+    );
+    ctx.stroke();
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(
+        198*textSize/30,//x
+        20*textSize/30,//y
+        22*textSize/30,//radius
+        -Math.PI/2,
+        Math.PI/2,
+        entry.wake.side == AFTER
+    );
+    ctx.stroke();
+    //Circles around bed time
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(
+        248*textSize/30,//x
+        20*textSize/30,//y
+        22*textSize/30,//radius
+        0,
+        2*Math.PI
+    );
+    ctx.stroke();
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(
+        248*textSize/30,//x
+        20*textSize/30,//y
+        22*textSize/30,//radius
+        -Math.PI/2,
+        Math.PI/2,
+        entry.bed.side == AFTER
+    );
+    ctx.stroke();
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 "use strict";
 
+//TODO: move this to its own place (it doesnt belong here, i dont think)
+let entry;
+
 function initCalendarJournal(){
     initSymbols();
     updateSymbolBank();
+    entry ??= new Entry();
+    repaintEntryCanvas(entry);
 }
 initCalendarJournal();
 
