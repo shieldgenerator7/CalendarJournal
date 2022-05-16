4944f61ed35ecd425f07807b3b0815e602ef7141
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-05-16 02:00:58 -0400

    Improve format of symbol buttons

--- src/DisplayManager.js
+++ src/DisplayManager.js
@@ -8,13 +8,14 @@ function updateSymbolBank(){
     let symbolBank = $("divBankSymbol");
     let buttonString = symbolNames
         .map(name => createSymbolButtonString(name))
-        .join("<br>");
+        .join("");
     symbolBank.innerHTML = buttonString;
 }
 
 function createSymbolButtonString(symbolName){
     return ""+
         `<button id="btnSymbol_${symbolName}" class="symbolButton"
+            title="${symbolName}"
             onclick="console.log('${symbolName} clicked')"
         >
             <img src="src/Assets/Sprites/${symbolName}.png" />
