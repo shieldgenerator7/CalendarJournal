2648e35fafaf49c2a4dae836b667282e0628b9ee
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-05-22 22:13:02 -0400

    Add Box and BoxManager

---
+++ src/Box.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const TYPE_TEXT = 1;
+const TYPE_IMAGE = 2;
+const TYPE_CONTAINER = 3;
+
+class Box{
+    constructor(content, position, size){
+        this.position = position ?? new Vector2();
+        this.size = size ?? new Vector2();
+        this.content = content;
+        this.type = getContentType(content);
+    }
+}
+
+function getContentType(content){
+    //Error checking
+    if (!content){
+        console.error("Must pass in some content!", content);
+        return undefined;
+    }
+    //Text
+    if (typeof content === "string"){
+        return TYPE_TEXT;
+    }
+    if (typeof content === "object"){
+        if (content instanceof Image){
+            return TYPE_IMAGE;
+        }
+        if (content instanceof Box){
+            return TYPE_CONTAINER;
+        }
+        //Default object return value
+        console.error("Content of unknown object type:", content.constructor.name);
+        return undefined;
+    }
+    //Default return value
+    console.error("Content of unknown type:", content);
+    return undefined;
+}
---
+++ src/BoxManager.js
@@ -0,0 +1,95 @@
+"use strict";
+
+let boxSet = {
+    boxes: [],
+}
+
+function updateBoxSet(entry, textSize, canvasSize){
+    //Error checking
+    if (!entry){
+        console.error("Invalid entry!", entry);
+        return;
+    }
+    //Defaults
+    textSize ??= 30;
+    canvasSize ??= 350;
+    //Initialization
+    let str = "";
+    let place = new Vector2(0,0);
+    let size = new Vector2(textSize,textSize);
+    //Header text date
+    str = `${entry.date}`;
+    size = new Vector2(textSize,textSize);
+    createBox(str, place, size);
+    place.x += size.x;
+    //Header text wake time
+    str = `${(""+entry.wake.time).padStart(2,"0")}`;
+    size = new Vector2(textSize,textSize);
+    createBox(str, place, size);
+    place.x += size.x;
+    //Header text bed time
+    str = `${(""+entry.bed.time).padStart(2,"0")}`;
+    size = new Vector2(textSize,textSize);
+    createBox(str, place, size);
+    place.x += size.x;
+    place.y += size.y;
+    //Place badges
+    let badgeLimitX = canvasSize - textSize;
+    size = new Vector2(textSize, textSize);
+    entry.badges.forEach((badge, i) => {
+        createBox(
+            symbols[badge].icon,
+            place,
+            size
+        );
+        place.x += textSize;
+        if (place.x >= badgeLimitX - textSize){
+            place.x = 0;
+            place.y += textSize;
+        }
+    });
+    place.y += textSize;
+    //Place pleasures
+    let place2 = new Vector2(badgeLimitX, 0);
+    size = new Vector2(textSize, textSize);
+    entry.pleasures.forEach((pleasure, i) => {
+        createBox(
+            symbols[pleasure].icon,
+            place2,
+            size
+        );
+        place2.y += textSize;
+    });
+    //Draw records
+    entry.records.forEach((record, i) => {
+        place.X = 0;
+        record.body.forEach((part, i) => {
+            //TODO: scale these parts down (or wrap it?) if it is too wide
+            if (!isPartSymbol(part)){
+                size = new Vector2(getTextSize(part), textSize);
+                createBox(
+                    part,
+                    place,
+                    size
+                );
+                place.x += size.x;
+            }
+            else{
+                size = new Vector2(textSize, textSize);
+                createBox(
+                    symbols[partToSymbol(part)].icon,
+                    place,
+                    size
+                );
+                place.x += size.x;
+            }
+        });
+        place.y += textSize;
+    });
+}
+
+function createBox(content, position, size){
+    let box = new Box(content, position, size);
+    boxSet.boxes.push(box);
+    return box;
+}
