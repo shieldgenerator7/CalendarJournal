dd2cfa52ed9c1b1faaca1c8ab758abe052de0e6b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-05-26 23:44:16 -0400

    Improve search functionality

--- src/SearchSymbol.js
+++ src/SearchSymbol.js
@@ -6,8 +6,21 @@ let searchResults = {
 }
 
 function filterSymbolNames(query){
+    //Sanitize query
+    query = query?.trim();
+    //Early exit
+    if (!query){
+        searchResults.query = query;
+        searchResults.symbols = symbolSet.symbols.slice();//shallow copy the array
+        return;
+    }
+    //Sanitize query pass 2
+    let queries = query.split(" ").filter(str => str?.trim());
+    query = queries.join(" ");
+    //Save sanitized query
     searchResults.query = query;
+    //Search using query
     searchResults.symbols = symbolSet.symbols.filter(
-        symbol => !query || symbol.name.includes(query)
+        symbol => queries.some(q => symbol.name.includes(q))
     );
 }
