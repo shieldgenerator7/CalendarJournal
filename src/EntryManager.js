1fe700a504a2458be9b42840bb51a26f40aac709
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-17 16:56:51 -0400

    Fill out EntryManager

--- src/EntryManager.js
+++ src/EntryManager.js
@@ -5,9 +5,12 @@ let entryList;
 let entryMap;
 
 function initEntryManager(){
-    loadEntry();    
+    //load entry list
+    loadEntry();
     //set entry
-    entry = entryList[entryList.length - 1] ?? new Entry();
+    entry = entryList[entryList.length - 1] ?? createEntry();
+    //init entryMap
+    updateEntryMap();
 }
 
 function saveEntry(){
@@ -23,5 +26,33 @@ function loadEntry(){
     entryList = JSON.parse(localStorage.getItem("CalendarJournal-entryList")) ?? [];
     entryList.forEach((e, i) => {
         validateEntry(e);
+    });
+}
+
+function getEntry(date){
+    let result = entryMap[date];
+    if (!result){
+        console.error(`Given date is not in the entryMap! ${date}`);
+    }
+    return result;
+}
+
+function createEntry(){
+    let newEntry = new Entry();
+    entryList.push(newEntry);
+    updateEntryMap();
+    return newEntry;
+}
+
+function updateEntryMap(){
+    entryMap = {};
+    entryList.forEach((e)=>{
+        let key = e.date;
+        //error checking
+        if (entryMap[key]){
+            console.error(`Date has more than one entry! ${key}. Using most recent one`);
+        }
+        //Add entry to map
+        entryMap[key] = e;
     });
 }
